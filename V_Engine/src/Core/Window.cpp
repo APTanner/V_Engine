dbacd16974d9d51b13b507162309a50c47c98a6e
lunchbox-boy <aydenptanner@gmail.com>
2023-09-27 17:33:19 -0500

    Windows in progress

--- V_Engine/src/Core/Window.cpp
+++ V_Engine/src/Core/Window.cpp
@@ -1,7 +1,30 @@
 #include "pch.h"
 #include "Window.h"
 
+#include "Core/Log.h"
+
 namespace V_Engine
 {
-	GLFWwindow 
+	bool Window::s_GLFWInitialized;
+
+	Window::Window(Window::Data data)
+	{
+		m_data = data;
+
+		LOG_DEBUG("Creating Window %s (%u, %u)", data.Title, data.Width, data.Height);
+
+		if (!s_GLFWInitialized)
+		{
+			int success = glfwInit();
+			if (!success)
+			{
+				LOG_ERROR("GLFW not initialized");
+			}
+
+			m_window = glfwCreateWindow((int)data.Width, (int)data.Height, data.Title.c_str(), nullptr, nullptr);)
+			glfwMakeContextCurrent(m_window);
+			glfwSetWindowUserPointer(m_window, &m_data);
+			SetVSync(data.VSync);
+		}
+	}
 }
--- V_Engine/src/Core/Window.h
+++ V_Engine/src/Core/Window.h
@@ -1,12 +1,50 @@
 #pragma once
 
+#include "pch.h"
+
 #include "GLFW/glfw3.h"
+#include "Core/Events/Event.h"
 
 namespace V_Engine
 {
-	struct WindowData
+	class Window
 	{
+	public:
+		using f_EventCallback = std::function<void(Event&)>;
 
+		struct Data
+		{
+			std::string Title;
+			unsigned int Width;
+			unsigned int Height;
+			bool VSync;
+
+			f_EventCallback EventCallback;
+
+			Data(const std::string& title = "V",
+				unsigned int width = 1920,
+				unsigned int height = 1080,
+				bool vSync = false)
+				: Title(title), Width(width), Height(height), VSync(vSync)
+			{
+			}
+		};
+
+		Window(Data data);
+
+		void OnUpdate();
+
+		inline unsigned int GetWidth() const { return m_data.Width; }
+		inline unsigned int GetHeight() const { return m_data.Height; }
+
+		void SetVSync(bool enabled);
+		bool IsVSync() const;
+
+		inline void SetEventCallback(const f_EventCallback& callback) { m_data.EventCallback = callback; }
+	private:
+		GLFWwindow* m_window;
+		Data m_data;
+		static bool s_GLFWInitialized;
 	};
 }
 
